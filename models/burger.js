853b3707ee6ecb77d067f2b46f33e204cb80fa3a
Sofia <sz14g@my.fsu.edu>
2017-11-06 15:42:36 -0600

    homework complete. still need to deploy to heroku

---
+++ models/burger.js
@@ -0,0 +1,27 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+// Create the code that will call the ORM functions using burger specific input for the ORM.
+var burger = {
+	selectAll: function(callback) {
+		orm.selectAll("burgers", function(result) {
+			callback(result);
+		});
+	},
+	// The variables cols and vals are arrays.
+	insertOne: function(cols, vals, callback) {
+		orm.insertOne("burgers", cols, vals, function(result) {
+			callback(result);
+		});
+	},
+	updateOne: function(objColVals, condition, callback) {
+		orm.updateOne("burgers", objColVals, condition, function(result) {
+			callback(result);
+		});
+	}
+};
+
+
+
+// Export the database functions for the controller (burgerController.js).
+module.exports = burger;
